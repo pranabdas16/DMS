b9f4098b9531136dcb5706198ec8d6ab71867422
Pranab Das <pranabdas16@gmail.com>
2018-07-06 10:49:42 -0400

    DMS application Structure

---
+++ DmsConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DmsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DmsMvcapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DmsMvcapp.MvcApplication" Language="C#" %>
